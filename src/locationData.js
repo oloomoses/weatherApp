27accf9be7945550c31f6455a65c68c8f87ddfec
oloomoses <olooine@gmail.com>
2020-12-15 20:30:56 +0300

    add a function that converts from celcius to fahrenheight

--- src/locationData.js
+++ src/locationData.js
@@ -1,7 +1,11 @@
 const getLocationData = async (city) => {
-  const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${process.env.API_KEY}&units=metric`);
-  const data = await response.json();
-  return data;
+  try {
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${process.env.API_KEY}&units=metric`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export { getLocationData as default };
--- src/ui.js
+++ src/ui.js
@@ -2,7 +2,7 @@ import getLocationData from './locationData';
 
 class UI {
   static searchFrom() {
-    const content = document.querySelector('.content');
+    // const content = document.querySelector('.content');
     const form = document.querySelector('form');
 
     const input = `<input class="form-control mr-sm-2 location-value" type="search" placeholder="Search" aria-label="Search">
@@ -14,38 +14,51 @@ class UI {
   static showData() {
     const location = document.querySelector('.location-value');
     const cityName = document.querySelector('.location-name');
-    const dateTime = document.querySelector('.time');
+    // const dateTime = document.querySelector('.time');
     const temp = document.querySelector('.temp');
     const weatherDescription = document.querySelector('.desc');
     const feels = document.querySelector('.feels-like');
     const humidity = document.querySelector('.humidity');
     const wind = document.querySelector('.wind');
+    const btn = document.querySelector('.checked-switch');
+    const errorDisp = document.querySelector('.error');
 
     getLocationData(location.value)
       .then(data => {
         cityName.textContent = `${data.name}, ${data.sys.country}`;
-        temp.textContent = `${data.main.temp}`;
-        feels.textContent = `Feels like: ${data.main.feels_like}`;
         weatherDescription.textContent = `${data.weather[0].description}`;
-        humidity.textContent = `Humidity: ${data.main.humidity}`;
+        humidity.textContent = `Humidity: ${data.main.humidity}%`;
         wind.textContent = `Wind: ${data.wind.speed} km/h`;
+        temp.innerHTML = `${data.main.temp}&deg;C`;
+        feels.innerHTML = `Feels like: ${data.main.feels_like}&deg;C`;
+
+        btn.addEventListener('change', () => {
+          if (btn.checked) {
+            setTimeout(() => {
+              const tempData = UI.toFahrenheit(data.main.temp);
+              const feelsLike = UI.toFahrenheit(data.main.feels_like);
+              temp.innerHTML = `${tempData}&deg;F`;
+              feels.innerHTML = `Feels like: ${feelsLike}&deg;F`;
+            }, 200);
+          } else {
+            setTimeout(() => {
+              temp.innerHTML = `${data.main.temp}&deg;C`;
+              feels.innerHTML = `Feels like: ${data.main.feels_like}&deg;C`;
+            }, 200);
+          }
+        });
+
         console.log(data);
       })
-      .catch(error => console.log(error));
+      .catch(error => {
+        errorDisp.innerHTML = error;
+      });
   }
 
   static toFahrenheit(temp) {
-    temp = parseFloat(temp);
-    temp = Math.round((temp = temp * 1.8 + 32));
+    temp = Math.round(temp * 1.8 + 32);
     return temp;
   }
-
-  static toCelsius(temp) {
-    temp = parseFloat(temp);
-    temp = Math.round((temp = (temp - 32) * (5 / 9)));
-    return temp;
-  }
-
 }
 
 export { UI as default };
