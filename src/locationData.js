a15c3e38af58d7806ebf17367be4f9a88daf22df
oloomoses <olooine@gmail.com>
2020-12-15 16:37:01 +0300

    add tuggle button

--- src/locationData.js
+++ src/locationData.js
@@ -1,5 +1,5 @@
 const getLocationData = async (city) => {
-  const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${process.env.API_KEY}`);
+  const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${process.env.API_KEY}&units=metric`);
   const data = await response.json();
   return data;
 };
--- src/ui.js
+++ src/ui.js
@@ -13,12 +13,39 @@ class UI {
 
   static showData() {
     const location = document.querySelector('.location-value');
+    const cityName = document.querySelector('.location-name');
+    const dateTime = document.querySelector('.time');
+    const temp = document.querySelector('.temp');
+    const weatherDescription = document.querySelector('.desc');
+    const feels = document.querySelector('.feels-like');
+    const humidity = document.querySelector('.humidity');
+    const wind = document.querySelector('.wind');
+
     getLocationData(location.value)
       .then(data => {
+        cityName.textContent = `${data.name}, ${data.sys.country}`;
+        temp.textContent = `${data.main.temp}`;
+        feels.textContent = `Feels like: ${data.main.feels_like}`;
+        weatherDescription.textContent = `${data.weather[0].description}`;
+        humidity.textContent = `Humidity: ${data.main.humidity}`;
+        wind.textContent = `Wind: ${data.wind.speed} km/h`;
         console.log(data);
       })
       .catch(error => console.log(error));
   }
+
+  static toFahrenheit(temp) {
+    temp = parseFloat(temp);
+    temp = Math.round((temp = temp * 1.8 + 32));
+    return temp;
+  }
+
+  static toCelsius(temp) {
+    temp = parseFloat(temp);
+    temp = Math.round((temp = (temp - 32) * (5 / 9)));
+    return temp;
+  }
+
 }
 
 export { UI as default };
